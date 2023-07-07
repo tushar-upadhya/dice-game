dce948344a856e75d7dcf4d7049d8e150938c7f6
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-07-08 00:20:55 +0530

    responsive remain ONLY

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -1,33 +1,35 @@
 import styled from "styled-components";
 import { Button } from "../styled/Button";
 
+// import diceImage from "../";
+
 const StartGame = ({ toggle }) => {
-  return (
-    <Container>
-      <div>
-        <img src="/images/dices.png" />
-      </div>
-      <div className="content">
-        <h1>Dice Game</h1>
-        <Button onClick={toggle}>Play Now</Button>
-      </div>
-    </Container>
-  );
+    return (
+        <Container>
+            <div>
+                <img src="/images/dices.svg" />
+            </div>
+            <div className="content">
+                <h1>Dice Game</h1>
+                <Button onClick={toggle}>Play Now</Button>
+            </div>
+        </Container>
+    );
 };
 
 export default StartGame;
 
 const Container = styled.div`
-  max-width: 1180px;
-  height: 100vh;
-  display: flex;
-  margin: 0 auto;
-  align-items: center;
+    max-width: 1180px;
+    height: 100vh;
+    display: flex;
+    margin: 0 auto;
+    align-items: center;
 
-  .content {
-    h1 {
-      font-size: 96px;
-      white-space: nowrap;
+    .content {
+        h1 {
+            font-size: 96px;
+            white-space: nowrap;
+        }
     }
-  }
 `;
